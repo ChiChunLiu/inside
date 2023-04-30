317af7a5eef34145c94aff3a7ff16d526c343f46
ccl <cliu@ccliu.me>
2023-04-30 20:35:56 +0800

    Add seventh chord generation

--- inside/main.py
+++ inside/main.py
@@ -10,7 +10,7 @@ def cli():
 
 
 @click.command()
-@click.option("--root", required=True, help="root note with alteratino in flat. e.g. Db")
+@click.option("--root", required=True, help="root note with alteration in flat. e.g. Db")
 @click.option("--name", required=True, help="scale name")
 def scale(root, name):
     notes = SequenceGenerator.generate_scale(Note[root], name)
@@ -18,13 +18,23 @@ def scale(root, name):
 
 
 @click.command()
-@click.option("--root", required=True, help="root note with alteratino in flat. e.g. Db")
-@click.option("--name", required=True, help="triad name")
-@click.option("--inversion", default=0, help="degree of inversion")
+@click.option("--root", required=True, help="root note with alteration in flat. e.g. Db")
+@click.option("--name", required=True, help="triad name. e.g aug")
+@click.option("--inversion", default=0, help="degree of inversion", type=click.IntRange(0, 2))
 def triad(root, name, inversion):
     notes = SequenceGenerator.generate_triad(Note[root], name, inversion)
     pprint_notes(notes, color=True)
 
 
+@click.command()
+@click.option("--root", required=True, help="root note with alteration in flat. e.g. Db")
+@click.option("--name", required=True, help="seventh name. e.g. min7b5")
+@click.option("--inversion", default=0, help="degree of inversion", type=click.IntRange(0, 3))
+def seventh(root, name, inversion):
+    notes = SequenceGenerator.generate_seventh(Note[root], name, inversion)
+    pprint_notes(notes, color=True)
+
+
 cli.add_command(scale)
 cli.add_command(triad)
+cli.add_command(seventh)
